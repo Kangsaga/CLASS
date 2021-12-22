902b1db530b6c6ed4c7db7f377524bac4a4a265a
beoumsu kang <95010758+Kangsaga@users.noreply.github.com>
2021-12-22 14:01:11 +0900

    Add files via upload

---
+++ 상속01전화/TellMain.java
@@ -0,0 +1,22 @@
+package 상속01;
+
+public class TellMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Tell tell = new Tell();
+		tell.Call();
+		tell.ButtonClick();
+		
+		//부모에 있는 기능을 전부 상속 받았기 때문에
+		//작성해 주지 않아도 기능이 사용가능하다
+		
+		Folder folder = new Folder();
+		folder.Call();
+		folder.Camera();
+		folder.ButtonClick();
+		
+	}
+
+}
